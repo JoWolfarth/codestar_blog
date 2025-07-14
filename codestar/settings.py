446e6f34e69b255098f05733b04bb2501b1aa798
JoWolfarth <jose.wolfarth@btinternet.com>
2025-07-14 16:02:45 +0100

    re-added .venv/ and env.py

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-g!6vwd^y&25_dl@g%7al1rlo25)484^_tuvuyvlsy79q$x^0-_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
