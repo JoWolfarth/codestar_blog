614d157891d32aece173d50d5f62d88274db6eac
JoWolfarth <jose.wolfarth@btinternet.com>
2025-07-18 11:21:03 +0100

    added summernote

--- blog/models.py
+++ blog/models.py
@@ -29,5 +29,5 @@ class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="commenter")
     body = models.TextField()
-    approved = models.BooleanField(False)
+    approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
--- blog/views.py
+++ blog/views.py
@@ -6,4 +6,4 @@ from .models import Post
 # Create your views here.
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1)
-    template = "post_list.html"
+    template_name = "post_list.html"
