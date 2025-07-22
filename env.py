3e8b2d88ffd8c13329b03569bfb1407a39c53414
JoWolfarth <jose.wolfarth@btinternet.com>
2025-07-22 16:40:27 +0100

    update SECRET_KEY in env.py and add gitignore file

--- env.py
+++ env.py
@@ -2,4 +2,4 @@ import os
 
 os.environ.setdefault(
     "DATABASE_URL", "postgresql://neondb_owner:npg_EdwTQFDH0a3x@ep-proud-boat-a20s4if4.eu-central-1.aws.neon.tech/awake_froth_punk_356309")
-os.environ.setdefault("SECRET_KEY", "z2dt82els#ssi6x(c6u!)@^ktg)4ii^dj6d^%xhb^jlz8g$a-c")
+os.environ.setdefault("SECRET_KEY", "583e3302219ee6e285418439a7bd9c01")
